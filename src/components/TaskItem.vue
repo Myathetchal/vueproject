7113f28309d31a6c131f52e37c20824ae38e0953
Myathetchal <myathetchal.199827@gmail.com>
2022-04-05 10:52:08 +0630

    css works

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,30 @@
+<template>
+     <li class="tasks__item">
+      <button :class="className"
+          @click.self="$emit('complete')"
+      >
+        {{ task.title }}
+      </button>
+      <button class="tasks__item__remove button alert pull-right"
+              @click="$emit('remove')"
+      >
+        <i class="fa fa-times"></i>
+      </button>
+    </li>
+</template>
+
+<script>
+export default {
+  name: 'TaskItem',
+  props: ['task'],
+  computed: {
+    className () {
+      let classes = ['tasks__item__toggle']
+      classes.push('tasks__item__toggle--completed')
+      return classes.join(' ')
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
